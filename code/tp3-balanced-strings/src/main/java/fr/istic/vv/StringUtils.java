f15e24875e5c953b1747a6007ac2ecac305f4698
lpro <leo.provost@etudiant.univ-rennes1.fr>
2023-03-10 09:35:12 +0100

    version recursive

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -1,11 +1,60 @@
 package fr.istic.vv;
 
-public class StringUtils {
+import java.util.Stack;
 
-    private StringUtils() {}
+public final class StringUtils{
+    static final Stack stack = new Stack();
 
-    public static boolean isBalanced(String str) {
-        return false;
+    public static void clearStack() {
+        stack.clear();
     }
 
+    public static boolean isBalanced(String testedString) {
+
+        if (testedString.length() == 0) {
+            if (stack.empty()) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } else {
+
+            if (testedString.charAt(0) != '[' && testedString.charAt(0) != '('
+                    && testedString.charAt(0) != '{') {
+
+                switch (testedString.substring(0, 1)) {
+                    case "]":
+                        if (stack.peek().toString().charAt(0) == '[') {
+                            stack.pop();
+                            return isBalanced(testedString.substring(1));
+                        } else {
+                            return false;
+                        }
+                    case "}":
+                        if (stack.peek().toString().charAt(0) == '{') {
+                            stack.pop();
+                            return isBalanced(testedString.substring(1));
+                        } else {
+                            return false;
+                        }
+                    case ")":
+                        if (stack.peek().toString().charAt(0) == '(') {
+                            stack.pop();
+                            return isBalanced(testedString.substring(1));
+                        } else {
+                            return false;
+                        }
+                    default:
+                        return false;
+                }
+
+            } else {
+
+                stack.add(testedString.charAt(0));
+                return isBalanced(testedString.substring(1));
+            }
+
+        }
+    }
 }
--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -1,11 +1,54 @@
 package fr.istic.vv;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static fr.istic.vv.StringUtils.isBalanced;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class StringUtilsTest {
+class balancedStringTest {
+    @BeforeEach
+    public void setup() {
+        fr.istic.vv.StringUtils.clearStack();
+    }
 
+    @Test
+    public void isBalancedTestNullInput() {
+        assertTrue(fr.istic.vv.StringUtils.isBalanced(""), "the empty string should return true");
+    }
 
+    @Test
+    public void isBalancedTestGrammar() {
+        assertFalse(fr.istic.vv.StringUtils.isBalanced("null"), "the grammar is only {,},[,],(,)");
+    }
+
+    @Test
+    public void isBalancedTestBalanced() {
+        assertTrue(fr.istic.vv.StringUtils.isBalanced("[[{()(){}}]]"), "a balanced string should return true");
+    }
+
+    @Test
+    public void isBalancedTestGrammarNonEmptyStack() {
+        assertFalse(fr.istic.vv.StringUtils.isBalanced("[[[[{}]]]"), "a not empty stack should return false");
+    }
+
+    @Test
+    public void isBalancedTestGrammarNonEmptyStack2() {
+        assertFalse(fr.istic.vv.StringUtils.isBalanced("[[[((({}))]]]"), "a not empty stack should return false");
+    }
+
+    @Test
+    public void isBalancedTestGrammarNonEmptyStack3() {
+        assertFalse(fr.istic.vv.StringUtils.isBalanced("({{{[[[(({{}}))]]]}})"), "a not empty stack should return false");
+    }
+
+    @Test
+    public void isBalancedTestTooManyClosingTags() {
+        assertFalse(fr.istic.vv.StringUtils.isBalanced("[[[{}())]]]"), "Too many closing tags should return false");
+    }
+
+    @Test
+    public void isBalancedTestLargeInput() {
+        assertTrue(fr.istic.vv.StringUtils.isBalanced("((((((((((((((((((((((([[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]])))))))))))))))))))))))"), "large number of calls should not crash");
+    }
 }
